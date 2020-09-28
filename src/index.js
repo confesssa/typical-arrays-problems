b48362a04fc3d1aa95d976129f7ca20fd3ea7780
volha <v.khviadchenia@gmail.com>
2020-09-29 00:39:52 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) && array.length !== 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+  
 }
 
 exports.max = function max (array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        return Math.max(...array);
+    } else 
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else
   return 0;
 }
